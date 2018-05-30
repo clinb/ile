e219d88a3a75e9b95aa0c68869691f9ece58779b
clinb <39760867+clinb@users.noreply.github.com>
2018-05-30 11:06:25 +0200

    Update Ile_Interdite.java

--- m2105/Ile_Interdite/src/ile_interdite/Ile_Interdite.java
+++ m2105/Ile_Interdite/src/ile_interdite/Ile_Interdite.java
@@ -17,7 +17,7 @@ public class Ile_Interdite {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        // truc
+        // truc truc
     }
     
 }

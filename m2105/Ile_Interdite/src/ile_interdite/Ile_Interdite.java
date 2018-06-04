aad1f31c9af61d101d37f9e6bf3d9ea0d385f7d6
clinb <benjaminclin@hotmail.fr>
2018-06-04 11:45:41 +0200

    aled

--- m2105/Ile_Interdite/src/ile_interdite/Ile_Interdite.java
+++ m2105/Ile_Interdite/src/ile_interdite/Ile_Interdite.java
@@ -16,6 +16,63 @@ public class Ile_Interdite {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
+    
+    public void tourDeJeu() {
+            
+    }
+    
+    public void choixAction(String nomAct) {
+        
+    }
+    
+    public void getPA(Joueur joueur) {
+        
+    }
+    
+    public boolean joueurCourant(String nomJoueur) {
+        return false;
+        
+    }
+    
+    public void assecher() {
+        
+    }
+    
+    public void deplacementPlongeur() {
+        
+    }
+    
+    public boolean rechercheTuileInondéeAdjacente(int coordonnéeAventurierX, int coordonnéeAventurierY) {
+        return false;
+        
+    }
+    
+    public void deplacement() {
+        
+    }
+    
+    public EtatTuile getEtatTuileAdj(Tuile tuile) {
+        
+    }
+    
+    public void resetPA(Joueur joueur) {
+        
+    }
+    
+    public void getPauseAdjJoueur(Joueur joueur) {
+        
+    }
+    
+    public void getCarteJoueur(Joueur joueur) {
+        
+    }
+    
+    public int getNbCarteJoueur(Joueur joueur) {
+        
+    }
+    
+    public 
     
 }
---
+++ m2105/Ile_Interdite/src/ile_interdite/Joueur.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ile_interdite;
+
+/**
+ *
+ * @author clinb
+ */
+public class Joueur {
+    private String nom;
+    private int numTourDeJeux;
+    private int posI;
+    private int posJ;
+    
+    public void choixAction(Action action) {
+        
+    }
+    
+    public void assecherTuile(Tuile tuile) {
+        
+    }
+    
+    public void setPA(int nbPA) {
+        
+    }
+    
+    public void seDeplacer(Tuile tuile) {
+        
+    }
+    
+    public void addCarte(CarteTresor carte) {
+        
+    }
+    
+    public void retraitCarte(CarteTresor carte) {
+        
+    }
+    
+    public void gagnerTresor(Tresor tresor) {
+        
+    }
+    
+    public void piocher2Cartes() {
+        
+    }
+    
+    public void utiliserCarteSpe(CarteTresor carte) {
+        
+    }
+    
+    public void defausserCarte(CarteTresor carte) {
+        
+    }
+    
+    public void resetPA() {
+        
+    }
+    
+    public void setPosJoueur(int posI, int posJ) {
+        
+    }
+    
+    public int getPosI() {
+        return posI;
+    }
+    
+    public int getPosJ() {
+        return posJ;
+    }
+    
+    public void donnerCarte(CarteTresor carte, Joueur joueur2) {
+        
+    }
+}
